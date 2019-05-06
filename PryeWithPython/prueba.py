b7d6379d5ca8eea09028d71a36ec0bd2e0ae0430
swilsonmelo <swilsonmelo@gmail.com>
2019-05-06 13:51:06 -0500

    graficando

--- PryeWithPython/prueba.py
+++ PryeWithPython/prueba.py
@@ -3,20 +3,14 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-
-def chiSquare():
+def chiSquare(df,beta):
     """
     hace la función de chi cuadrado y su grafica
+    df = grados de libertad
+    beta = nivel de confianza
     """
-
-    ##grados de libertad
-    print("inserte los grados de libertad")
-    df = int(stdin.readline().strip())
-
-    print("inserte el nivel de confianza, valor mayor que 0 y menor que 1")
-    beta = float(stdin.readline().strip())
+    
     criticalFin = stats.chi2.ppf(0.99, df)
-
     fig,ax = plt.subplots(1,1)
 
     #print(criticalFin)
@@ -44,66 +38,57 @@ def chiSquare():
 
     if(df == 1):
         yLimi = 1
-
     plt.title('Distribución chi Cuadrado')
     plt.xlabel('valores Chi Cuadrado')
     plt.xlim(0,xLimi)
     plt.ylim(0,yLimi)
     plt.show()
 
-
-def normalEstandar():
+def normalEstandar(mu, sigma, alfa, tipo):
     """
     Actualmente dado un alfa saca los dos lados del alfa
+    mu = media
+    sigma = variación estandar
+    alfa = area bajo la curva
+    tipo:
+    -1 area de la izquierda
+    0 area de izquierda y derecha
+    1 area de la derecha
     """
 
-    print("Vamos a graficar la normal estandar")
-    print("Digitar el valor para miu")
-    mu = float(stdin.readline().strip())
-    print("Digitar el valor para sigma")
-    sigma = float(stdin.readline().strip())
-    print("Digitar el valor para alfa")
-    alfa = float(stdin.readline().strip())
-    mu, sigma = 0, 1 # media y desvio estandar
-    #alfa = 0.4
+    #mu, sigma = 0, 1 # media y desvio estandar
+    #alfa = 0.4j
     normal = stats.norm(mu, sigma)
     x = np.linspace(normal.ppf(0.00001),normal.ppf(0.99999), 10000)
     fp = normal.pdf(x) # Función de Probabilidad
     plt.plot(x, fp)
     plt.title('Distribución Normal')
-    
     plt.xlabel('valores')
-    critical = normal.ppf(alfa)
-    plt.fill_betweenx(fp, critical*-1, x, where = x >= critical*-1)
-    plt.fill_betweenx(fp, critical, x, where = x <= critical)
+    criticalL = normal.ppf(alfa)
+    criticalR =criticalL + 2*(mu-criticalL)
+    if(tipo == 1 or tipo == 0):
+        plt.fill_betweenx(fp, criticalR, x, where = x >= criticalR)
+    if(tipo == -1 or tipo == 0):
+        plt.fill_betweenx(fp, criticalL, x, where = x <= criticalL)
     plt.show()
-    print(critical)
+    print(criticalL,criticalR)
 
-
-def tStudent():
+def tStudent(df, alfa, tipo):
     """
     Graficando t de Student
     """
-
-    print("inserte los grados de libertad")
-    df = float(stdin.readline().strip())
-    print("inserte el alfa")
-    alfa = float(stdin.readline().strip())
-    #df = 15 # parametro de forma.
     t = stats.t(df)
-    
     x = np.linspace(t.ppf(0.000001),t.ppf(0.999999), 10000)
     fp = t.pdf(x) # Función de Probabilidad
     plt.plot(x, fp)
     critical = t.ppf(alfa)
-    print(critical)
-    plt.fill_betweenx(fp, critical*-1, x, where = x >= critical*-1)
-    plt.fill_betweenx(fp, critical, x, where = x <= critical)
+    if(tipo == 1 or tipo == 0):
+        plt.fill_betweenx(fp, critical*-1, x, where = x >= critical*-1)
+    if(tipo == -1 or tipo == 0):
+        plt.fill_betweenx(fp, critical, x, where = x <= critical)
     plt.title('Distribución t de Student')
-    
     plt.xlabel('valores')
     plt.show()
-
 
 def main():
     print("digite 1 para chicuadrado")
@@ -111,10 +96,43 @@ def main():
     print("digite 3 t Student")
     n = int(stdin.readline().strip())
     if(n == 1):
-        chiSquare()
-    elif(n == 2):
-        normalEstandar()
-    elif(n == 3):
-        tStudent()
+        print("inserte los grados de libertad (valor minimo 1)")
+        df = int(stdin.readline().strip())
+        print("inserte el nivel de confianza, valor mayor que 0 y menor que 1")
+        beta = float(stdin.readline().strip())
+        chiSquare(df,beta)
 
+    elif(n == 2):
+        print("Digitar el valor para miu")
+        mu = float(stdin.readline().strip())
+        print("Digitar el valor para sigma")
+        sigma = float(stdin.readline().strip())
+        print("Digitar el valor para alfa")
+        alfa = float(stdin.readline().strip())
+        print("I para el area a la izquierda, D para el area a la derecha y B para ambas areas ")
+        tipo = stdin.readline().strip()
+        print(tipo)
+        if(tipo == "I"):
+            lados = -1
+        elif(tipo == "B"):
+            lados = 0
+        else:
+            lados = 1
+        normalEstandar(mu,sigma,alfa,lados)
+
+    elif(n == 3):
+        print("inserte los grados de libertad(valor minimo 1)")
+        df = float(stdin.readline().strip())
+        print("inserte el alfa")
+        alfa = float(stdin.readline().strip())
+        print("I para el area a la izquierda, D para el area a la derecha y B para ambas areas ")
+        tipo = stdin.readline().strip()
+        print(tipo)
+        if(tipo == "I"):
+            lados = -1
+        elif(tipo == "B"):
+            lados = 0
+        else:
+            lados = 1
+        tStudent(df, alfa, lados)
 main()
